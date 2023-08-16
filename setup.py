09541300bdc4560e3b5d9138665383a6e90b3176
Shivansh Dwivedi <shivansh2283@gmail.com>
2023-08-16 11:04:36 +0530

    Updated Long Description

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.1'
-DESCRIPTION = 'Python Mailing Library'
-LONG_DESCRIPTION = 'A package that allows to send mails from python environment'
+VERSION = "1.0.2"
+DESCRIPTION = "Python Mailing Library"
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="mailpad",
@@ -10,18 +11,19 @@ setup(
     author="Shivansh Dwivedi",
     author_email="<me@shivanshdwivedi.tech>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://mailpad.tech/",
     packages=find_packages(),
     install_requires=['langchain'],
     keywords=['mailpad','python', 'mail', 'email', 'email client', 'smtp'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
     ],
     license="MIT",
 )
